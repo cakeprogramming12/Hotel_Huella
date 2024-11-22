a5a683e246096582ad3a21ddc83f9c3a52c1bb40
Ramiro Vega <110796808+cakeprogramming12@users.noreply.github.com>
2024-11-21 22:34:38 -0600

    Initial commit

---
+++ Principal.php
@@ -0,0 +1,274 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Holiday Inn Express Toluca</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+    <!-- Bootstrap icons-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="css/Principal.css" rel="stylesheet" />
+</head>
+
+<body>
+    <!-- Responsive navbar-->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container px-5">
+            <a class="navbar-brand" href="#!">Bienvenido al Holiday Inn Express Toluca</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="index.php">Inicio</a></li>
+                    <li class="nav-item"><a class="nav-link" href="aboutus.php">Acerca de nosotros</a></li>
+                    <li class="nav-item"><a class="nav-link" href="contactus.php">Contacto</a></li>
+                    <li class="nav-item"><a class="nav-link" href="gallery.php">Galeria</a></li>
+                    <li class="nav-item"><a class="nav-link" href="dineandlounge.php">Almuerzo y cena</a></li>
+                    <li class="nav-item"><a class="nav-link" href="rulesandregulation.php">Reglas Y Reglamentos</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- Header-->
+    <header class="bg-dark py-5">
+        <div class="container px-5">
+            <div class="row gx-5 justify-content-center">
+                <div class="col-lg-6">
+                    <div class="text-center my-5">
+                        <h1 class="display-5 fw-bolder text-white mb-2">Holiday Inn Express Toluca</h1>
+                        <p class="lead text-white-50 mb-4">Te ofrecemos una experiencia única de confort y lujo en cada
+                            detalle. Nuestro hotel, ubicado en el corazón de Toluca, combina elegancia moderna con el
+                            encanto de un servicio personalizado. </p>
+                        <div class="d-grid gap-3 d-sm-flex justify-content-sm-center">
+                            <a class="btn btn-primary btn-lg px-4 me-sm-3" href="index.php">Iniciar sesion</a>
+                            <a class="btn btn-outline-light btn-lg px-4" href="aboutus.php">Más de nosotros</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- Features section-->
+    <section class="py-5 border-bottom" id="features">
+        <div class="container px-5 my-5">
+            <div class="row gx-5">
+                <div class="col-lg-4 mb-5 mb-lg-0">
+                    <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-building"></i>
+                    </div>
+                    <h2 class="h4 fw-bolder">Habitacion Estándar</h2>
+                    <p>Perfecta para aquellos que buscan comodidad y simplicidad, nuestra Habitación Estándar ofrece
+                        todo lo que necesitas para una estancia relajante.</p>
+                    <a class="text-decoration-none" href="#pricing">
+
+                        <i class="bi bi-arrow-right"></i>
+                    </a>
+                </div>
+                <div class="col-lg-4 mb-5 mb-lg-0">
+                    <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-building"></i>
+                    </div>
+                    <h2 class="h4 fw-bolder">Habitación Deluxe</h2>
+                    <p>Un nivel superior de confort, la Habitación Deluxe está diseñada para quienes desean un poco más
+                        de espacio y lujo durante su estancia.</p>
+                    <a class="text-decoration-none" href="#pricing1">
+                        Ir
+                        <i class="bi bi-arrow-right"></i>
+                    </a>
+                </div>
+                <div class="col-lg-4">
+                    <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-building"></i>
+                    </div>
+                    <h2 class="h4 fw-bolder">Suite Ejecutiva</h2>
+                    <p>La opción más exclusiva de nuestro hotel, la Suite Ejecutiva combina lujo y funcionalidad en un
+                        espacio elegante. Perfecta para largas estancias o viajes de negocios de alto nivel.</p>
+                    <a class="text-decoration-none" href="#pricing2">
+                        Ir
+                        <i class="bi bi-arrow-right"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Pricing section-->
+    <section class="bg-light py-5 border-bottom" id="pricing">
+        <div class="container px-5 my-5">
+            <div class="text-center mb-5">
+                <h2 class="fw-bolder">Precios Especiales</h2>
+                <p class="lead mb-0">Te mostramos los precios de nuestras habitaciones</p>
+            </div>
+            <div class="row gx-5 justify-content-center">
+                <!-- Pricing card free-->
+                <div class="col-lg-6 col-xl-4">
+                    <div class="card mb-5 mb-xl-0">
+                        <div class="card-body p-5">
+                            <div class="small text-uppercase fw-bold text-muted">Habitacion Estándar
+                            </div>
+                            <div class="mb-3">
+                                <span class="display-4 fw-bold">$500 MXN</span>
+                                <span class="text-muted">/ la noche.</span>
+                            </div>
+                            <ul class="list-unstyled mb-4">
+                                <li class="mb-2">
+                                    <i class="bi bi-check text-primary"></i>
+                                    <strong>2 personas</strong>
+                                </li>
+                                <li class="mb-2">
+                                    <i class="bi bi-check text-primary"></i>
+                                    Wi-Fi gratuito
+                                </li>
+                                <li class="mb-2">
+                                    <i class="bi bi-check text-primary"></i>
+                                    Baño privado con ducha
+                                </li>
+                                <li class="mb-2">
+                                    <i class="bi bi-check text-primary"></i>
+                                    TV de pantalla plana con canales por cable
+                                </li>
+                                <li class="mb-2 text-muted">
+                                    <i class="bi bi-x"></i>
+                                    Caja fuerte
+                                </li>
+                                <li class="mb-2 text-muted">
+                                    <i class="bi bi-x"></i>
+                                    Aire acondicionado y calefacción
+                                </li>
+                                <li class="mb-2 text-muted">
+                                    <i class="bi bi-x"></i>
+                                    Vista panorámica de la ciudad
+                                </li>
+                                <li class="text-muted">
+                                    <i class="bi bi-x"></i>
+                                    Jacuzzi privado en el baño
+                            </ul>
+                            <div class="d-grid"><a class="btn btn-outline-primary" href="reservation.php">Quiero
+                                    esta!</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Pricing card free-->
+                <div class="col-lg-6 col-xl-4">
+                    <div class="card mb-5 mb-xl-0">
+                        <div class="card-body p-5">
+                            <div class="small text-uppercase fw-bold text-muted">Habitación Deluxe</div>
+                            <div class="mb-3">
+                                <span class="display-4 fw-bold">$700 MXN</span>
+                                <span class="text-muted">/ la noche.</span>
+                            </div>
+                            <ul class="list-unstyled mb-4">
+                                <li class="mb-2">
+                                    <i class="bi bi-check text-primary"></i>
+                                    <strong>3 personas</strong>
+                                </li>
+                                <li class="mb-2">
+                                    <i class="bi bi-check text-primary"></i>
+                                    Wi-Fi gratuito
+                                </li>
+                                <li class="mb-2">
+                                    <i class="bi bi-check text-primary"></i>
+                                    Baño privado con ducha
+                                </li>
+                                <li class="mb-2">
+                                    <i class="bi bi-check text-primary"></i>
+                                    TV de pantalla plana con canales por cable
+                                </li>
+                                <li class="mb-2 text-muted">
+                                    <i class="bi bi-x"></i>
+                                    Caja fuerte
+                                </li>
+                                <li class="mb-2 text-muted">
+                                    <i class="bi bi-x"></i>
+                                    Aire acondicionado y calefacción
+                                </li>
+                                <li class="mb-2 text-muted">
+                                    <i class="bi bi-x"></i>
+                                    Vista panorámica de la ciudad
+                                </li>
+                                <li class="text-muted">
+                                    <i class="bi bi-x"></i>
+                                    Jacuzzi privado en el baño
+                            </ul>
+                            <div class="d-grid"><a class="btn btn-outline-primary" href="reservation.php">Quiero
+                                    esta!</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Pricing card pro-->
+                <div class="col-lg-6 col-xl-4" id="pricing1">
+                    <div class="card mb-5 mb-xl-0">
+                        <div class="card-body p-5">
+                            <div class="small text-uppercase fw-bold">
+                                <i class="bi bi-star-fill text-warning"></i>
+                                Suite Ejecutiva
+                            </div>
+                            <div class="mb-3">
+                                <span class="display-4 fw-bold">$900 MXN</span>
+                                <span class="text-muted">/ la noche.</span>
+                                <ul class="list-unstyled mb-4">
+                                    <li class="mb-2">
+                                        <i class="bi bi-check text-primary"></i>
+                                        <strong>2 personas</strong>
+                                    </li>
+                                    <li class="mb-2">
+                                        <i class="bi bi-check text-primary"></i>
+                                        Cama King Size con ropa de cama de lujo
+                                    </li>
+                                    <li class="mb-2">
+                                        <i class="bi bi-check text-primary"></i>
+                                        Amplia área de trabajo con escritorio
+                                    </li>
+                                    <li class="mb-2">
+                                        <i class="bi bi-check text-primary"></i>
+                                        Wi-Fi de alta velocidad
+                                    </li>
+                                    <li class="mb-2">
+                                        <i class="bi bi-check text-primary"></i>
+                                        Smart TV de 50 pulgadas
+                                    </li>
+                                    <li class="mb-2">
+                                        <i class="bi bi-check text-primary"></i>
+                                        Minibar
+                                    </li>
+                                    <li class="mb-2">
+                                        <i class="bi bi-check text-primary"></i>
+                                        Baño privado con ducha y artículos de aseo premium
+                                    </li>
+                                    <li class="text-muted">
+                                        <i class="bi bi-x"></i>
+                                        Servicio de habitaciones personalizado
+                                    </li>
+                                </ul>
+                                <div class="d-grid"><a class="btn btn-primary" href="reservation.php">Quiero esta!</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Pricing card enterprise-->
+                </div>
+            </div>
+    </section>
+
+
+    <!-- Footer-->
+    <footer class="py-5 bg-dark">
+        <div class="container px-5">
+            <p class="m-0 text-center text-white">Copyright &copy; 2024</p>
+        </div>
+
+    </footer>
+    <!-- Bootstrap core JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Core theme JS-->
+    <script src="js/scripts.js"></script>
+
+    <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+     
+</body>
+
+</html>
